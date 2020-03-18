4d3d7058df8fbb1b284b65aeabdd33842a44cc41
Ben-Nachmanson <bennachmanson@gmail.com>
2020-03-17 19:53:17 -0700

    added sound

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from tkinter import *
 from pynput.keyboard import Listener
-window = Tk()
+import threading
+from playsound import playsound
 
 
 def on_press(key):
@@ -12,7 +13,15 @@ def on_press(key):
             key))
 
 
-def clicked():
+def is_integer(value: str, *, base: int = 10) -> bool:
+    try:
+        int(value, base=base)
+        return True
+    except ValueError:
+        return False
+
+
+def enterClicked():
     userMin = minBox.get()
 
     if is_integer(userMin) == True:
@@ -25,22 +34,34 @@ def clicked():
     print(userMin)
 
 
-def is_integer(value: str, *, base: int = 10) -> bool:
-    try:
-        int(value, base=base)
-        return True
-    except ValueError:
-        return False
+def bellClicked():
+
+    playsound('Tram-bell-sound-effect.mp3')
+    print("bell clicked")
 
 
-def clickCheck(key):
+def timeControl():
+
+    playsound('Tram-bell-sound-effect.mp3')
+    print("time done")
+
+
+def startClicked():
+    print("start clicked")
+    timer = threading.Timer(10, timeControl)
+    timer.start()
+
+
+def keyboardListener(key):
     # keyData = str(key)
     # print(keyData)
-    print("Button Clicked")
+    print("Keyboard Clicked")
 
 
+window = Tk()
 minBox = 0
 
+# tkinter gui button and label declarations
 window.geometry('500x300')
 
 window.title("Relax")
@@ -55,14 +76,22 @@ minBox.grid(column=1, row=0)
 minutes = Label(window, text="minutes", font=("Ariel", 10))
 minutes.grid(column=2, row=0)
 
-enter = Button(window, text="Enter", command=clicked)
+enter = Button(window, text="Enter", command=enterClicked)
 enter.grid(column=3, row=0)
 
-intCheck = Label(window, text="", font=("Ariel", 8))
+sound = Label(window, text="Sound:", font=("Ariel", 14))
+sound.grid(column=0, row=1)
 
+bell = Button(window, text="Bell", command=bellClicked)
+bell.grid(column=1, row=1)
+
+intCheck = Label(window, text="", font=("Ariel", 8))
 intCheck.grid(column=4, row=0)
 
 
-with Listener(on_press=clickCheck) as l:
+start = Button(window, text="Start", command=startClicked)
+start.place(x=460, y=270)
+
+with Listener(on_press=keyboardListener) as l:
     window.mainloop()
     l.join()
